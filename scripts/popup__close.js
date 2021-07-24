ee6fd4ae106494902a43059cc8023d9e9633abbe
Yan <yanbystrikov@ya.ru>
2021-07-24 23:48:44 +0300

    удалены лишние папки и файлы

--- scripts/popup__close.js
+++
@@ -1,8 +0,0 @@
-let closeButton = document.querySelector ('.popup__close');
-
-function closePopup () {
-    let popupClosed = document.querySelector ('.popup');
-    popupClosed.classList.remove ('popup_opened');
-}
-
-closeButton.addEventListener('click', closePopup);
--- scripts/popup__open.js
+++
@@ -1,9 +0,0 @@
-let profileEdit = document.querySelector ('.profile__edit-button');
-
-function func () {
-   let popupOpened = document.querySelector('.popup');
-   popupOpened.classList.add ('popup_opened');
-}
-
-profileEdit.addEventListener('click',func);
- 
